5707b8e8665492983b87cbd9199f5a33f4ba310b
horouuu <horou@coronne.io>
2026-05-11 09:31:40 +0800

    feat: checkpet

---
+++ src/commands/checkpet.ts
@@ -0,0 +1,23 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import { CommandContext, Command } from "../bot.types";
+
+const checkpetData = new SlashCommandBuilder()
+  .setName("checkpet")
+  .setDescription('"');
+
+const checkpet = {
+  ...checkpetData.toJSON(),
+  execute: async (cmdContext: CommandContext) => {
+    const { interaction } = cmdContext;
+    if (!interaction.isChatInputCommand()) return;
+
+    try {
+      await interaction.reply({
+        content: "Pong!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } catch (e) {}
+  },
+} satisfies Command;
+
+export { checkpet };
